8c38e3e22818c9e70fa6ebdeaf35f917cc002d8b
makuto72 <makuto@hotmail.it>
2014-12-11 09:51:29 +0100

    funzione getfeatures

--- Source.cpp
+++ Source.cpp
@@ -52,190 +52,206 @@ void getdata(ifstream &in, vector<trip>& v, vector<vector<trip>>& vaction ){
 	while(getline(in, line)){
 		trip tmp;
 		double devNull;
-			double t1,t2,t3;
-			stringstream ss(line);
+		double t1,t2,t3;
+		stringstream ss(line);
 
-			ss >> t1 >> t2 >> t3 >> devNull;
-			tmp.x=t1;
-			tmp.y=t2;
-			tmp.z=t3;
+		ss >> t1 >> t2 >> t3 >> devNull;
+		tmp.x=t1;
+		tmp.y=t2;
+		tmp.z=t3;
 
-			v.push_back(tmp);
+		v.push_back(tmp);
 
-			if(v.size()==20){
-							vaction.push_back(v);
-							v.clear();
-						}
+		if(v.size()==20){
+			vaction.push_back(v);
+			v.clear();
+		}
 	}
 }
 
-
-int main(){
-
-	bool singolo = true;
-	string nome_file;
-	ifstream in;
-	vector<trip> v;
-	vector<vector<trip>> vaction;
-
-	//--------------------------------------Acquisizione del dataset-------------------------------------
-	if(!singolo){
-		for(int i=0; i<num_a_train; ++i){
-
-			stringstream ssi;
-			ssi << setfill('0') << setw(2) << i+1;		
-
-			for(int k=0; k<num_s_train; ++k){
-
-				stringstream ssk;
-				ssk << setfill('0') << setw(2) << k+1;
-
-				for(int j=0; j<num_e_train; ++j){
-
-					stringstream ssj;
-					ssj << setfill('0') << setw(2) << j+1;
-
-					//Creo nome file da aprire
-					nome_file += "dataset/";
-					nome_file += "a" + ssi.str();
-					nome_file += "_s" + ssk.str();
-					nome_file += "_e" + ssj.str();
-					nome_file += "_skeleton3d.txt";
-					cout << "Aperto file: " << nome_file << endl;
-
-					//Apro il file
-					in.open(nome_file);
-					if(!in.is_open())
-						cout << "Errore apertura file!" << endl;
-
-					//Ottengo tutto il contenuto del file -> vettore di triplette (x, y, z)
-					getdata(in, v, vaction);
-
-					//Ogni vaction contiene tanti elementi quanti frame memorizzati (ogni frame 20 triplette)
-					cout << "Sono stati memorizzati " << vaction.size() << " frame" << endl;
-					nome_file.clear();
-					ssj.str().clear();
-				}
-				ssk.clear();
-			}	
-			ssi.clear();
-		}
-	}
-	else{// Versione per caricare un singolo file del dataset
-		stringstream ssi;
-		ssi << setfill('0') << setw(2) << num_a;
-		stringstream ssk;
-		ssk << setfill('0') << setw(2) << num_s;
-		stringstream ssj;
-		ssj << setfill('0') << setw(2) << num_e;
-
-		//Creo nome del file da aprire
-		nome_file += "dataset/";
-		nome_file += "a" + ssi.str();
-		nome_file += "_s" + ssk.str();
-		nome_file += "_e" + ssj.str();
-		nome_file += "_skeleton3d.txt";
-		cout << "Aperto singolo file: " << nome_file << endl;
-
-		//Apro il file
-		in.open(nome_file);
-		if(!in.is_open())
-			cout << "Errore apertura file!" << endl;
-
-		//Ottengo tutto il contenuto del file -> vettore di triplette (x, y, z)
-		getdata(in, v, vaction);
-
-		cout << "Sono stati memorizzati " << vaction.size() << " frame" << endl;
-	}
-
-	//--------------------------------Creazione del Feature Vector-------------------------------
+//Funzione che data l'azione (vettore di frame) riempie il vettore di features calcolate
+void getfeatures(vector<vector<trip>> vaction, vector<vector<double>>& vfeatures){
 	vector<double> feat;
-	vector<vector<double>> vfeatures;
-
-	cout << "Calcolo le features..." << endl;
 	for(int i=0; i<vaction.size(); ++i){ //per ogni frame calcolo la feature
-		double dist;
+			double dist;
 
-		//Distanza euclidea mani
-		dist = sqrt(pow((getxyz(vaction, i, right_hand).x-getxyz(vaction, i, left_hand).x), 2) +
-			pow((getxyz(vaction, i, right_hand).y-getxyz(vaction, i, left_hand).y), 2) +
-			pow((getxyz(vaction, i, right_hand).z-getxyz(vaction, i, left_hand).z), 2));
+			//Distanza euclidea mani
+			dist = sqrt(pow((getxyz(vaction, i, right_hand).x-getxyz(vaction, i, left_hand).x), 2) +
+				pow((getxyz(vaction, i, right_hand).y-getxyz(vaction, i, left_hand).y), 2) +
+				pow((getxyz(vaction, i, right_hand).z-getxyz(vaction, i, left_hand).z), 2));
+			//Accumulo la feature
+			feat.push_back(dist);
 
-		feat.push_back(dist);
+			//Distanza mano destra - testa
+			dist = sqrt(pow((getxyz(vaction, i, right_hand).x-getxyz(vaction, i, head).x), 2) +
+				pow((getxyz(vaction, i, right_hand).y-getxyz(vaction, i, head).y), 2) +
+				pow((getxyz(vaction, i, right_hand).z-getxyz(vaction, i, head).z), 2));
+			//Accumulo la feature
+			feat.push_back(dist); 
 
-		//Distanza mano destra - testa
-		dist = sqrt(pow((getxyz(vaction, i, right_hand).x-getxyz(vaction, i, head).x), 2) +
-			pow((getxyz(vaction, i, right_hand).y-getxyz(vaction, i, head).y), 2) +
-			pow((getxyz(vaction, i, right_hand).z-getxyz(vaction, i, head).z), 2));
+			//Distanza mano sinistra - testa
+			dist = sqrt(pow((getxyz(vaction, i, left_hand).x-getxyz(vaction, i, head).x), 2) +
+				pow((getxyz(vaction, i, left_hand).y-getxyz(vaction, i, head).y), 2) +
+				pow((getxyz(vaction, i, left_hand).z-getxyz(vaction, i, head).z), 2));
+			//Accumulo la feature
+			feat.push_back(dist); 
 
-		feat.push_back(dist); 
+			//Memorizzo tutte le features calcolate
+			vfeatures.push_back(feat);
 
-		//Distanza mano sinistra - testa
-		dist = sqrt(pow((getxyz(vaction, i, left_hand).x-getxyz(vaction, i, head).x), 2) +
-			pow((getxyz(vaction, i, left_hand).y-getxyz(vaction, i, head).y), 2) +
-			pow((getxyz(vaction, i, left_hand).z-getxyz(vaction, i, head).z), 2));
+			//Pulisco il vettore features per accumularne di nuove
+			feat.clear();
+		}
+}
 
-		feat.push_back(dist); 
+int main(int argc, char *argv[]){
+	
+	if(strcmp(argv[1], "train")==0){
 
-		//Memorizzo tutte le features calcolate
-		vfeatures.push_back(feat);
+		bool singolo = true;
+		string nome_file;
+		ifstream in;
+		vector<trip> v;
+		vector<vector<trip>> vaction;
 
-		feat.clear();
+		//--------------------------------------Acquisizione del dataset-------------------------------------
+		if(!singolo){
+			for(int i=0; i<num_a_train; ++i){
+
+				stringstream ssi;
+				ssi << setfill('0') << setw(2) << i+1;		
+
+				for(int k=0; k<num_s_train; ++k){
+
+					stringstream ssk;
+					ssk << setfill('0') << setw(2) << k+1;
+
+					for(int j=0; j<num_e_train; ++j){
+
+						stringstream ssj;
+						ssj << setfill('0') << setw(2) << j+1;
+
+						//Creo nome file da aprire
+						nome_file += "dataset/";
+						nome_file += "a" + ssi.str();
+						nome_file += "_s" + ssk.str();
+						nome_file += "_e" + ssj.str();
+						nome_file += "_skeleton3d.txt";
+						std::cout << "Aperto file: " << nome_file << endl;
+
+						//Apro il file
+						in.open(nome_file);
+						if(!in.is_open())
+							std::cout << "Errore apertura file!" << endl;
+
+						//Ottengo tutto il contenuto del file -> vettore di triplette (x, y, z)
+						getdata(in, v, vaction);
+
+						//Ogni vaction contiene tanti elementi quanti frame memorizzati (ogni frame 20 triplette)
+						std::cout << "Sono stati memorizzati " << vaction.size() << " frame" << endl;
+						nome_file.clear();
+						ssj.str().clear();
+					}
+					ssk.clear();
+				}	
+				ssi.clear();
+			}
+		}
+		else{// Versione per caricare un singolo file del dataset
+			stringstream ssi;
+			ssi << setfill('0') << setw(2) << num_a;
+			stringstream ssk;
+			ssk << setfill('0') << setw(2) << num_s;
+			stringstream ssj;
+			ssj << setfill('0') << setw(2) << num_e;
+
+			//Creo nome del file da aprire
+			nome_file += "dataset/";
+			nome_file += "a" + ssi.str();
+			nome_file += "_s" + ssk.str();
+			nome_file += "_e" + ssj.str();
+			nome_file += "_skeleton3d.txt";
+			std::cout << "Aperto singolo file: " << nome_file << endl;
+
+			//Apro il file
+			in.open(nome_file);
+			if(!in.is_open())
+				std::cout << "Errore apertura file!" << endl;
+
+			//Ottengo tutto il contenuto del file -> vettore di triplette (x, y, z)
+			getdata(in, v, vaction);
+
+			std::cout << "Sono stati memorizzati " << vaction.size() << " frame" << endl;
+		}
+
+		//--------------------------------Creazione del Feature Vector-------------------------------
+		vector<vector<double>> vfeatures;
+
+		std::cout << "Calcolo le features..." << endl;
+		getfeatures(vaction, vfeatures);
+
+		std::cout << "Il feature vector ha " << vfeatures.size() << " vettori di features, con " << vfeatures[0].size() << " features" << endl;
+
+
+
+		//--------------------------------------------TRAINING HMM-----------------------------------------
+		int num_stati = 8;
+		int dim_fv = vfeatures[0].size(); //dimensione del singolo feature vector (tutti uguali)
+		int num_gaus = 1;
+		typedef vector<vector<double>>::iterator iter_vf;
+		const iter_vf ivf_init = vfeatures.begin();
+		const iter_vf ivf_final = vfeatures.end();
+
+		//Nuovo HMM
+		std::cout << "Costruisco un nuovo HMM..." << endl;
+		CHMM_GMM *phmm = new CHMM_GMM(num_stati, dim_fv, num_gaus);
+
+		//Inizializzo HMM
+		std::cout << "Inizializzo il feature vector..." << endl;
+		int num_iter;
+		//phmm->RandomInit(); 
+		phmm->Init_Equi(ivf_init, ivf_final);
+
+		//Addestramento HMM
+		std::cout << "Training..." << endl << endl;
+		int pniter;
+		double plogprobinit, plogprobfinal;
+		phmm->BaumWelch(ivf_init, ivf_final, &pniter, &plogprobinit, &plogprobfinal);
+		std::cout << "Valore iniziale: " << plogprobinit << endl;
+		std::cout << "Valore finale: " << plogprobfinal << endl;
+
+
+		//Ottengo valori HMM addestrato
+		Mat_<double> A; //Matrice transizioni
+		A = phmm->m_A;
+
+		//--------------------------------------------TESTING HMM---------------------------------------
+
+		//LogLikelihood: ottengo score da HMM date le osservazioni
+		std::cout << "logLikelihood ottenuta: " << phmm->LogLikelihood(ivf_init, ivf_final, &A) << endl;
+
+		//Salvo HMM: ottengo un char significativo essendo il salvataggio in C
+		string prefix = "hmm_";
+		stringstream suffix;
+		suffix << "_" << num_stati << "_" << num_gaus;
+		nome_file.erase(0, 8);
+		nome_file.erase(11, nome_file.size());
+		prefix.append(nome_file);
+		prefix.append(suffix.str());
+		char * cstr = new char [prefix.length()+1];
+		strcpy_s(cstr, prefix.length()+1, prefix.c_str());
+
+		//Salvo con un nome collegato all'azione considerata
+		phmm->SaveToFile(cstr);
+
+
+		in.close();
+		std::system("pause");
+
 	}
+	else{
+		std::cout << "testing" << endl;
 
-	cout << "Il feature vector ha " << vfeatures.size() << " vettori di features, con " << vfeatures[0].size() << " features" << endl;
-
-
-
-	//--------------------------------------------TRAINING HMM-----------------------------------------
-	int num_stati = 8;
-	int dim_fv = vfeatures[0].size(); //dimensione del singolo feature vector (tutti uguali)
-	int num_gaus = 1;
-	typedef vector<vector<double>>::iterator iter_vf;
-	const iter_vf ivf_init = vfeatures.begin();
-	const iter_vf ivf_final = vfeatures.end();
-	
-	//Nuovo HMM
-	cout << "Costruisco un nuovo HMM..." << endl;
-	CHMM_GMM *phmm = new CHMM_GMM(num_stati, dim_fv, num_gaus);
-
-	//Inizializzo HMM
-	cout << "Inizializzo il feature vector..." << endl;
-	int num_iter;
-	//phmm->RandomInit(); 
-	phmm->Init_Equi(ivf_init, ivf_final);
-
-	//Addestramento HMM
-	cout << "Training..." << endl << endl;
-	int pniter;
-	double plogprobinit, plogprobfinal;
-	phmm->BaumWelch(ivf_init, ivf_final, &pniter, &plogprobinit, &plogprobfinal);
-	cout << "Valore iniziale: " << plogprobinit << endl;
-	cout << "Valore finale: " << plogprobfinal << endl;
-	
-
-	//Ottengo valori HMM addestrato
-	Mat_<double> A; //Matrice transizioni
-	A = phmm->m_A;
-
-	//--------------------------------------------TESTING HMM---------------------------------------
-	
-	//LogLikelihood: ottengo score da HMM date le osservazioni
-	cout << "logLikelihood ottenuta: " << phmm->LogLikelihood(ivf_init, ivf_final, &A) << endl;
-
-	//Salvo HMM: ottengo un char significativo essendo il salvataggio in C
-	string prefix = "hmm_";
-	nome_file.erase(0, 8);
-	nome_file.erase(11, nome_file.size());
-	prefix.append(nome_file);
-	char * cstr = new char [prefix.length()+1];
-	strcpy_s(cstr, prefix.length()+1, prefix.c_str());
-
-	//Salvo con un nome collegato all'azione considerata
-	phmm->SaveToFile(cstr);
-	
-
-	in.close();
-	system("pause");
-
+		std::system("pause");
+	}
 }
